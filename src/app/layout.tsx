f47fa0be2b7efb40bd8d413bf7920b3bd7831630
Maxim Lugovskoy <lugovskou.myu@yandex.ru>
2025-12-01 11:07:06 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="ru" suppressHydrationWarning>
       <head>
         <meta name="yandex-verification" content="8f2bae575aa86202" />
-        <script id="yandex-metrika-counter" strategy="beforeInteractive">
+        <Script id="yandex-metrika-counter" strategy="beforeInteractive">
           {`
             (function(m,e,t,r,i,k,a){
                 m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
@@ -37,20 +37,21 @@ export default function RootLayout({
 
             ym(105475213, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
           `}
-        </script>
-        <noscript>
-          <div>
-            <img src="https://mc.yandex.ru/watch/105475213" style={{position:'absolute', left:-9999}} alt="" />
-          </div>
-        </noscript>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-95QGC7HGHE"></script>
-        <script>
+        </Script>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-95QGC7HGHE"></Script>
+        <Script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
           gtag('config', 'G-95QGC7HGHE');
-        </script>
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/105475213" style={{position:'absolute', left:-9999}} alt="" />
+          </div>
+        </noscript>
+        
       </head>
       <body
         className={`${exo_2.variable} ${geistMono.variable} antialiased w-full justify-center items-center`}
