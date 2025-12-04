363cd947c4e72552f54a271b25c4d11a8e01edd3
Максим Луговской <lugovskou.myu@yandex.ru>
2025-12-04 11:15:00 +0500

    `Removed unused components and files from shared/browse directory`

--- src/shared/browse/index.ts
+++ src/shared/browse/index.ts
@@ -2,4 +2,3 @@ export { default as ChapterSearchInput } from "./chapter-search-input";
 export { default as TitlePageSkeleton } from "./title-page-skeleton";
 export { default as TitleHeader } from "./title-header";
 export { default as TitleActions } from "./title-actions";
-export {default as TitlePageComponent} from "./title-page-components";
